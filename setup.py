f8c72a76c57c101a49a6c23dc8e8be3c05b4d0b4
Marco Favorito <marco.favorito@gmail.com>
2018-05-02 16:03:46 +0200

    0.1.0.post3

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MarcoFavorito/rltg',
-    version='0.1.0post2',
+    version='0.1.0post3',
     zip_safe=False,
 )
