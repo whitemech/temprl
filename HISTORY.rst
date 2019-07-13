ef5c22b16346b1ebb8a9862070f0c0bf07e5a152
MarcoFavorito <marco.favorito@gmail.com>
2019-07-13 13:00:01 -0700

    bump version to 0.2.0.

--- HISTORY.rst
+++ HISTORY.rst
@@ -4,6 +4,10 @@ History
 
 0.1.0 (2018-04-04)
 ------------------
-.. TODO
 
 * First release on PyPI.
+
+0.2.0 (2019-07-13)
+------------------
+
+* Main refactoring of the package.
--- temprl/__version__.py
+++ temprl/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "temprl"
 __description__ = "Framework for Reinforcement Learning with Temporal Goals."
 __url__ = "https://github.com/marcofavorito/temprl.git"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Marco Favorito"
 __author_email__ = "marco.favorito@gmail.com"
 __license__ = "MIT license"
