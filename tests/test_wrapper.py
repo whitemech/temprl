bcbe0bc8e3719f7261895640359c8076c4c7cfc3
MarcoFavorito <marco.favorito@gmail.com>
2019-11-09 23:08:17 +0100

    allow temporal goal initialization from DFA. Fixes #15

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -55,6 +55,50 @@ class TestWrapper:
         """Tear the tests down."""
 
 
+class TestWrapperFromDFA:
+    """Test that the wrapper built from a DFA works as expected."""
+
+    @classmethod
+    def setup_class(cls):
+        """Set the tests up."""
+        cls.env = GymTestEnv(n_states=5)
+
+        cls.formula = LDLfParser()("<(!s4)*;s3;(!s4)*;s0;(!s4)*;s4>tt")
+        cls.tg = TemporalGoal(
+            automaton=cls.formula.to_automaton(),
+            reward=10.0,
+            labels={"s0", "s1", "s2", "s3", "s4"},
+            reward_shaping=True,
+            extract_fluents=None
+        )
+        cls.wrapped = TemporalGoalWrapper(env=cls.env, temp_goals=[cls.tg], feature_extractor=None)
+
+    def test_observation_space(self):
+        """Test that the combined observation space is computed as expected."""
+        assert self.wrapped.observation_space == MultiDiscrete((5, 6))
+
+    def test_temporal_goal_reward(self):
+        """Test that the 'reward' property of the temporal goal works correctly."""
+        assert self.tg.reward == 10.0
+
+    def test_temporal_goal_formula(self):
+        """Test that the 'formula' property of the temporal goal works correctly."""
+        assert self.tg.formula is None
+
+    def test_temporal_goal_automaton(self):
+        """Test that the 'automaton' property of the temporal goal works correctly."""
+        assert isinstance(self.tg.automaton, RewardDFA)
+
+    def test_extract_fluents_raises_exception(self):
+        """Test that 'extract_fluents' raises 'NotImplementedError' if not set properly."""
+        with pytest.raises(NotImplementedError):
+            self.tg.extract_fluents(None, None)
+
+    @classmethod
+    def teardown_class(cls):
+        """Tear the tests down."""
+
+
 class TestWrapperRewardShaping:
     """This class tests the case when we apply reward shaping."""
 
@@ -67,6 +111,114 @@ class TestWrapperRewardShaping:
             reward=10.0,
             labels={"s0", "s1", "s2", "s3", "s4"},
             reward_shaping=True,
+            zero_terminal_state=True,
+            extract_fluents=lambda obs, action: PLInterpretation({"s" + str(obs[0])})
+        )
+        cls.wrapped = TemporalGoalWrapper(env=cls.env, temp_goals=[cls.tg], feature_extractor=None)
+
+    def test_reward_shaping(self):
+        """Test that the reward shaping works as expected."""
+        obs = self.wrapped.reset()
+        total_reward = 0
+        assert np.array_equal(obs, [0, 0])
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+
+        # s1
+        obs, reward, done, info = self.wrapped.step(2)
+        total_reward += reward
+        assert reward == 0
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s2
+        obs, reward, done, info = self.wrapped.step(2)
+        total_reward += reward
+        assert reward == 0
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s3 - positive reward
+        obs, reward, done, info = self.wrapped.step(2)
+        total_reward += reward
+        assert np.isclose(reward, 3.3333, rtol=1e-9, atol=0.0001)
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s2
+        obs, reward, done, info = self.wrapped.step(0)
+        total_reward += reward
+        assert reward == 0
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s1
+        obs, reward, done, info = self.wrapped.step(0)
+        total_reward += reward
+        assert reward == 0
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s0 - positive reward
+        obs, reward, done, info = self.wrapped.step(0)
+        total_reward += reward
+        assert np.isclose(reward, 3.3333, rtol=1e-9, atol=0.0001)
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s1
+        obs, reward, done, info = self.wrapped.step(2)
+        total_reward += reward
+        assert reward == 0
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s2
+        obs, reward, done, info = self.wrapped.step(2)
+        total_reward += reward
+        assert reward == 0
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s3
+        obs, reward, done, info = self.wrapped.step(2)
+        total_reward += reward
+        assert reward == 0
+        assert not self.tg.is_true()
+        assert not self.tg.is_failed()
+        # s4
+        obs, reward, done, info = self.wrapped.step(2)
+        total_reward += reward
+        assert np.isclose(reward, 4.3333, rtol=1e-9, atol=0.0001)
+        assert total_reward == 11.0
+        assert done
+        assert not self.tg.is_failed()
+        assert self.tg.is_true()
+
+    def test_when_temporal_goal_is_failed(self):
+        """Test the case when the temporal goal is failed."""
+        obs = self.wrapped.reset()
+        assert np.array_equal(obs, [0, 0])
+
+        # s1
+        obs, reward, done, info = self.wrapped.step(2)
+        assert reward == 0
+        # s2
+        obs, reward, done, info = self.wrapped.step(2)
+        assert reward == 0
+        # s3 - positive reward
+        obs, reward, done, info = self.wrapped.step(2)
+        assert np.isclose(reward, 3.3333, rtol=1e-9, atol=0.0001)
+        # s4 - temporal goal fails.
+        obs, reward, done, info = self.wrapped.step(2)
+        assert np.isclose(reward, 1 - 3.3333, rtol=1e-9, atol=0.0001)
+        assert self.tg.is_failed()
+
+
+class TestWrapperRewardShapingFromDFA:
+    """This class tests reward shaping when we build the temporal goal from the DFA."""
+
+    @classmethod
+    def setup_class(cls):
+        """Set the tests up."""
+        cls.env = GymTestObsWrapper(n_states=5)
+        cls.tg = TemporalGoal(
+            automaton=LDLfParser()("<(!s4)*;s3;(!s4)*;s0;(!s4)*;s4>tt").to_automaton(labels={"s0", "s1", "s2", "s3", "s4"}),
+            reward=10.0,
+            reward_shaping=True,
+            zero_terminal_state=True,
             extract_fluents=lambda obs, action: PLInterpretation({"s" + str(obs[0])})
         )
         cls.wrapped = TemporalGoalWrapper(env=cls.env, temp_goals=[cls.tg], feature_extractor=None)
