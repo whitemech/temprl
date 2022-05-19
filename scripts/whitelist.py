ed2bf039eb5bb9828bd3001a7f0b9b54eef449a9
MarcoFavorito <marco.favorito@gmail.com>
2022-05-19 16:38:17 +0200

    lint: fix vulture checks.

--- scripts/whitelist.py
+++ scripts/whitelist.py
@@ -1,7 +1,13 @@
 # flake8: noqa
 # type: ignore
 # pylint: skip-file
-logger  # unused variable (/home/marcofavorito/workfolder/temprl/temprl/automata.py:32)
-start_state  # unused variable (/home/marcofavorito/workfolder/temprl/temprl/automata.py:45)
-logger  # unused variable (/home/marcofavorito/workfolder/temprl/temprl/wrapper.py:38)
-TemporalGoalWrapper  # unused class (/home/marcofavorito/workfolder/temprl/temprl/wrapper.py:95)
+logger  # unused variable (temprl/automata.py:32)
+start_state  # unused variable (temprl/automata.py:45)
+logger  # unused variable (temprl/wrapper.py:38)
+TemporalGoalWrapper  # unused class (temprl/wrapper.py:95)
+RewardAutomaton  # unused class (temprl/reward_machines/automata.py:32)
+mcs  # unused variable (temprl/reward_machines/base.py:46)
+_.get_transitions  # unused method (temprl/reward_machines/base.py:101)
+logger  # unused variable (temprl/wrapper.py:34)
+_.automaton  # unused property (temprl/wrapper.py:59)
+TemporalGoalWrapper  # unused class (temprl/wrapper.py:127)
